2bedd584c2ab9e42b17836f8ff163a3d62f301fe
nanliyu <nancarpli@163.com>
2017-07-12 22:33:39 +0800

    chapter5

---
+++ src/initialization/BananPeel.java
@@ -0,0 +1,19 @@
+package initialization;
+
+/**
+ * Created by nanca on 7/12/2017.
+ */
+class Banana {
+    void peel(int i) {
+
+    }
+}
+
+public class BananPeel {
+    public static void main(String[] args) {
+        Banana a = new Banana();
+        Banana b = new Banana();
+        a.peel(1);
+        b.peel(2);
+    }
+}
---
+++ src/initialization/Leaf.java
@@ -0,0 +1,22 @@
+package initialization;
+
+/**
+ * Created by nanca on 7/12/2017.
+ */
+public class Leaf {
+    int i = 0;
+
+    Leaf increment() {
+        i++;
+        return this;
+    }
+
+    void print() {
+        System.out.println("i = " + i);
+    }
+
+    public static void main(String[] args) {
+        Leaf x = new Leaf();
+        x.increment().increment().increment().print();
+    }
+}
