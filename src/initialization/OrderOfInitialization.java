9810f4dd26e1dcf58899690623e0196801431e6c
nanliyu <nancarpli@163.com>
2017-07-13 22:53:25 +0800

    chapter5

---
+++ src/initialization/OrderOfInitialization.java
@@ -0,0 +1,34 @@
+package initialization;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * Created by nanca on 7/13/2017.
+ */
+class Window {
+    Window(int marker) {
+        print("Window(" + marker + ")");
+    }
+}
+
+class House {
+    Window w1 = new Window(1); // Before constructor
+    House() {
+        // Show that we're in the constructor:
+        print("House()");
+        w3 = new Window(33); // Reinitialize w3
+    }
+
+    Window w2 = new Window(2); // After constructor
+    void f() {
+        print("f()");
+    }
+    Window w3 = new Window(3); // At end
+}
+
+public class OrderOfInitialization {
+    public static void main(String[] args) {
+        House h = new House();
+        h.f(); // Shows that construction is done
+    }
+}
