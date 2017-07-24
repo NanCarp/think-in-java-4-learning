face1e1c40cae092d28e152da705646f4bd64226
NanCarp <nancarpli@163.com>
2017-07-24 15:52:11 +0800

    chapter9

---
+++ src/interfaces/TestRandVals.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import static net.mindview.util.Print.*;
+
+public class TestRandVals {
+    public static void main(String[] args) {
+        print(RandVals.RANDOM_INT);
+        print(RandVals.RANDOM_LONG);
+        print(RandVals.RANDOM_FLOAT);
+        print(RandVals.RANDOM_DOUBLE);
+    }
+
+}
