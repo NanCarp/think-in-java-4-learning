98afc1562710627f49e18db36e8fe771276b9dae
NanCarp <nancarpli@163.com>
2017-07-27 10:45:22 +0800

    1

---
+++ src/innerclasses/Callbacks.java
@@ -0,0 +1,13 @@
+// Using inner classes for callbacks
+package innerclasses;
+
+import static net.mindview.util.Print.*;
+
+interface Incrementable {
+    void increment();
+}
+
+
+public class Callbacks {
+    
+}
--- src/innerclasses/README.md
+++ src/innerclasses/README.md
@@ -3,3 +3,9 @@ Parcel6.java
 
 p198  
 Parcel9.java  
+
+p201  
+Parcel11.java  
+
+p204  
+为什么需要内部类  
