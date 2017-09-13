834a40d23a0dabd8c71337f4d7d45aa300fd8b60
NanCarp <nancarpli@163.com>
2017-09-13 17:12:40 +0800

    1

---
+++ src/containers/MapDataTest.java
@@ -0,0 +1,18 @@
+package containers;
+
+import net.mindview.util.Generator;
+import net.mindview.util.Pair;
+
+class Letters implements Generator<Pair<Integer, String>> {
+
+    @Override
+    public Pair<Integer, String> next() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
+
+public class MapDataTest {
+
+}
