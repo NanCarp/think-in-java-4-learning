4de442e24bc328cf67364fa672b2bf2ef7a70f05
NanCarp <nancarpli@163.com>
2017-09-28 13:55:22 +0800

    1

---
+++ src/io/ZipCompress.java
@@ -0,0 +1,11 @@
+package io;
+
+/**
+ * @ClassName: ZipCompress.java
+ * @Description: Uses Zip compression to compress any
+ * number of files given on the command line.
+ * {TODO
+ */
+public class ZipCompress {
+
+}
